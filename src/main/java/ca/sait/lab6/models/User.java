6cc30609c044ba4bc50a077de795d7313e23b289
Bluenurples <andy.diep@edu.sait.ca>
2022-03-08 14:41:16 -0700

    Created the user class

---
+++ src/main/java/ca/sait/lab6/models/User.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sait.lab6.models;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author andyd
+ */
+public class User implements Serializable {
+    
+}
